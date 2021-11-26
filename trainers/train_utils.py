419c3146fefa340b5b98abed8523c30eca339e22
Simon Amtoft Pedersen <simon.ipsum@gmail.com>
2021-11-26 12:16:31 +0100

    enable plotting

--- trainers/train_utils.py
+++ trainers/train_utils.py
@@ -1,3 +1,9 @@
+import os
+import torch
+import wandb
+import matplotlib.pyplot as plt
+
+
 class DeterministicWarmup(object):
     """ Linear deterministic warm-up """
     def __init__(self, n=100, t_max=1):
@@ -14,4 +20,23 @@ class DeterministicWarmup(object):
         return self.t
 
 
+def log_images(x_recon: torch.Tensor, x_sample: torch.Tensor, epoch: int) -> None:
+    """Log reconstruction and sample to wandb"""
+    name = f'./log_images/img_{epoch}.png'
 
+    # create plots
+    f, ax = plt.subplots(1, 2, figsize=(16, 6))
+    ax[0].plot(x_recon[:, 0], x_recon[:, 1], '.')
+    ax[0].set_title('Reconstruction')
+    ax[1].plot(x_sample[:, 0], x_sample[:, 1], '.')
+    ax[1].set_title('Sample', fontsize=16)
+    f.savefig(name, transparent=True, bbox_inches='tight')
+    plt.close()
+
+    # log plots
+    wandb.log({
+        "visualization": wandb.Image(name)
+    }, commit=True)
+
+    # remove logged images
+    os.remove(name)
